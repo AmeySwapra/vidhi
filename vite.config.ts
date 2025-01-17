7d8545ed18d5945db16d960207d9e7075a1007e9
AmeySwapra <ameyswapratechnologies@gmail.com>
2025-01-17 23:50:32 +0530

    lets try

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['problematic-package'], // Add the package name here
+      external: ['problematic-package'], 
     },
   },
 });
